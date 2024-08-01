500de9bec0f93c4be56a81fd9f36b797ac16bd2b
Redzwinger <redzwinger@outlook.com>
2024-08-01 09:24:21 +0530

    :(((((((((((((((((((((((((((((((((((((((((((((((((((((((

--- Computer Vision/R030 Achintya Kamath Computer Vision Experiment 3.py
+++ Computer Vision/R030 Achintya Kamath Computer Vision Experiment 3.py
@@ -11,6 +11,8 @@ Task 2: Change brightness and repeat task 1. Observe the effect on HoG due to ch
 Task 3: Use SVM detector to detect people
 '''
 
+# Tasks 1 and 2 #
+
 # Importing things #
 import matplotlib.pyplot as plt
 import numpy as np
@@ -20,4 +22,125 @@ from skimage.transform import resize # Used to resize the image to make it into 
 from skimage.feature import hog
 from skimage import exposure # Used to rescale the image without changing HOG
 
+def HistogramOfGradients(pix, ori):
+    RAWimage = imread("C:\Achintya\Achintya's Stuff\Gitty Things\Academia Stash\Academia-Stash\Computer Vision\cat.png") # Reading the image using skimage.io.imread
 
+    # Checking the shape #
+    print("\n Original Image Shape: ", RAWimage.shape)
+
+    # Plotting the original image #
+    plt.title("Original Raw Image")
+    plt.imshow(RAWimage)
+    plt.show()
+
+    darkenedImage = cv.convertScaleAbs(RAWimage, beta=-100)
+    Resizedimage = resize(RAWimage, (128*5.8, 64*7.7)) 
+    darkenedImage = resize(darkenedImage, (128*5.8, 64*7.7)) 
+    # Resizing to closest to the original size. This is done to enhance the HoG features. #
+
+    print("\n Resized Image Shape: ", Resizedimage.shape, darkenedImage.shape)
+
+    plt.subplot(1, 2, 1)
+    plt.title("Original Resized Image")
+    plt.imshow(Resizedimage)
+
+    plt.subplot(1, 2, 2)
+    plt.title("Darkened Resized Image")
+    plt.imshow(darkenedImage)
+    plt.show()
+
+    # Applying HoG Filter to the image #
+    filteredImage, HOGim_normal = hog(Resizedimage, orientations=ori, pixels_per_cell=pix, cells_per_block=(2, 2), visualize=True, channel_axis=(-1))
+
+    darkenedFilteredImage, HOGim_dark = hog(darkenedImage, orientations=ori, pixels_per_cell=pix, cells_per_block=(2, 2), visualize=True, channel_axis=(-1))
+
+    print("\n Shape of the Histogram Vector: ", filteredImage.shape, darkenedFilteredImage.shape)
+
+    plt.subplot(1, 2, 1)
+    plt.title("HoG Normal")
+    plt.imshow(HOGim_normal, cmap='gray')
+
+    plt.subplot(1, 2, 2)
+    plt.title("HoG Darkened")
+    plt.imshow(HOGim_dark, cmap='gray')
+    plt.show()
+
+    # Finding percentiles #
+    p2_1, p90_1 = np.percentile(HOGim_normal, (2,90))
+    p2_2, p90_2 = np.percentile(HOGim_dark, (2,90))
+
+    # Rescaling the images using Exposure #
+    HOG_normal_re = exposure.rescale_intensity(HOGim_normal, in_range=(p2_1, p90_1))
+    HOG_dark_re = exposure.rescale_intensity(HOGim_dark, in_range=(p2_2, p90_2))
+    
+    #Ploting Rescaled HOG images #
+    
+    plt.subplot(1, 2, 1)
+    plt.title("HoG Normal")
+    plt.imshow(HOG_normal_re, cmap='gray')
+
+    plt.subplot(1, 2, 2)
+    plt.title("HoG Darkened")
+    plt.imshow(HOG_dark_re, cmap='gray')
+    plt.show()
+
+    # Ploting Everything #
+    plt.subplot(1, 4, 1)
+    plt.title("Original Image")
+    plt.imshow(Resizedimage)
+
+    plt.subplot(1, 4, 2)
+    plt.title("Darkened Image")
+    plt.imshow(darkenedImage)
+
+    plt.subplot(1, 4, 3)
+    plt.title("HoG Normal")
+    plt.imshow(HOG_normal_re, cmap='gray')
+
+    plt.subplot(1, 4, 4)
+    plt.title("HoG Darkened")
+    plt.imshow(HOG_dark_re, cmap='gray')
+    plt.show()
+
+# Task 3 #
+
+def SVM_Detector():
+    
+    RAWimage = cv.imread("C:\Achintya\Achintya's Stuff\Gitty Things\Academia Stash\Academia-Stash\Computer Vision\people.png") # Reading the image using cv2.imread
+    
+    HOg = cv.HOGDescriptor()
+    
+    HOg.setSVMDetector(cv.HOGDescriptor_getDefaultPeopleDetector())
+    
+    if RAWimage.shape[1]<400:
+        (rows, cols) = RAWimage.shape[:2]
+        ratio = cols/float(rows)
+        
+        RAWimage = cv.resize(RAWimage, (400, int(cols*rows)))
+    
+    
+
+if __name__ == "__main__":
+    # Default Orientation is 9 #
+    orientations = 9
+    # Default Pixel Size is 8x8 #
+    pix_size = (8,8)
+    # HoG #
+    HistogramOfGradients(pix_size, orientations) 
+
+
+
+'''
+Conclusion 1:
+The Histogram of Gradients of the original image and the dark image are found and it is observed that the HoG of both the images are similar. This is because the HoG normalizes all of the features. This is one of the advantages of HoG, i.e., it is invariant to change in brightness.
+
+For number of pixels per cell = 8x8,
+Length of the HoG Descriptor is 196560.
+
+For number of pixels per cell = 16x16,
+Length of the HoG Descriptor is reducing to 46980.
+
+Therefore, smaller the cell size, larger the HoG Descriptor Vector.
+'''
+
+# Cautiously Crafted By Achintya Kamath/Redzwinger #
